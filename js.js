ec2ac11339262b756950e1fcf0f053ff0a379c57
snehasantosh <73293246+snehasantosh@users.noreply.github.com>
2023-07-04 16:07:28 +0530

    funcn.js

---
+++ js.js
@@ -0,0 +1,9 @@
+function setValues() {
+    var inputValue1 = "Value 1";
+    var inputValue2 = "Value 2";
+    var inputValue3 = "Value 3";
+  
+    document.getElementById("frstNm").value = inputValue1;
+    document.getElementById("nameOfOrg").value = inputValue2;
+    document.getElementById("nameOfOrg").value = inputValue3;
+  }
